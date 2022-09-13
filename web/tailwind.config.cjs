48286cd4adbd3462692116c87eda7bd29a169dac
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-09-13 18:45:17 -0300

    Stage 2/6

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,10 +5,14 @@ module.exports = {
     "./index.html"
   ],
   theme: {
+    fontFamily: {
+      "sans": ["Inter", "sans-serif"]
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
-        "nlw-gradient": "linear-gradient(89.86deg, #9572FC 0%, #43E7AD 50.52%, #E1D55D 100%)"
+        "nlw-gradient": "linear-gradient(89.86deg, #9572FC 0%, #43E7AD 50.52%, #E1D55D 100%)",
+        "game-gradient": "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)"
       }
     },
   },
