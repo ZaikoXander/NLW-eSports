dca8c4a09c782eb4815c53381d2ff63f2de171c5
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-09-13 14:26:33 -0300

    Stage 1/6

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.tsx",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        "nlw-gradient": "linear-gradient(89.86deg, #9572FC 0%, #43E7AD 50.52%, #E1D55D 100%)"
+      }
+    },
+  },
+  plugins: [],
+}
