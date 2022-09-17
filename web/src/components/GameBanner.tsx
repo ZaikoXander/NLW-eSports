718191a60f129756d7f58277091168d647a5c90e
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-09-17 16:31:59 -0300

    Stage 4/6

---
+++ web/src/components/GameBanner.tsx
@@ -0,0 +1,21 @@
+interface GameBannerProps {
+  bannerUrl: string
+  title: string
+  adsCount: number
+}
+
+export default function GameBanner({ bannerUrl, title, adsCount }: GameBannerProps) {
+  return (
+    <a href="" className="relative rounded-lg overflow-hidden">
+      <img src={bannerUrl} alt="" />
+      {/* Image source */}
+
+      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+        <strong className="font-bold text-white block">{title}</strong>
+        {/* Game title */}
+        <span className="text-zinc-300 text-sm block">{adsCount} Anúncio(s)</span>
+        {/* Ads counts */}
+      </div>
+    </a>
+  )
+}
